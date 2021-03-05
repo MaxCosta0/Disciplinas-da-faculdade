5a123fd2cd6375822ae78f56802d0e90fa2bba97
MaxCosta0 <maxleysoares006@gmail.com>
2021-03-05 15:56:46 -0300

    Exercicios 2 a 5 finalizados

---
+++ ProgramacaoFuncional/Semana1/Exercicios2a5.hs
@@ -0,0 +1,11 @@
+--2)
+dobro x = 2*x
+
+--3)
+quadruplo x = dobro((dobro x)) 
+
+--4)
+hipotenusa cateto1 cateto2 = sqrt(cateto1 ** 2 + cateto2 ** 2)
+
+--5)
+distancia xa ya xb yb = sqrt( (xb-xa)^2 + (yb-ya)^2 )
